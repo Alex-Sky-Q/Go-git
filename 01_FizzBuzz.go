f2acc2a6a043f316f556dfdba9e921547f23f43e
AlexSky <83851934+Alex-Sky-Q@users.noreply.github.com>
2022-10-04 05:44:04 +0300

    Solved with "for" and "switch"

--- 01_FizzBuzz.go
+++ 01_FizzBuzz.go
@@ -1,1 +1,26 @@
+/*
+Print the numbers between 1 and 20, however
+- If the number is divisible by 3, print "fizz" instead
+- If the number is divisible by 5, print "buzz" instead
+- If the number is divisible by 3 and 5, print "fizz buzz" instead
+- Otherwise print the number
+*/
+
 package main
+
+import "fmt"
+
+func main() {
+	for i := 1; i <= 20; i++ {
+		switch {
+		case i%3 == 0 && i%5 == 0:
+			fmt.Println("fizz buzz")
+		case i%3 == 0:
+			fmt.Println("fizz")
+		case i%5 == 0:
+			fmt.Println("buzz")
+		default:
+			fmt.Println(i)
+		}
+	}
+}
