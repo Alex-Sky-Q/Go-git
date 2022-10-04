4ab694960abf571a21d9a4b6626cb00fd30c681c
AlexSky <83851934+Alex-Sky-Q@users.noreply.github.com>
2022-10-04 23:50:46 +0300

    Solution improvement

--- 06_square-struct.go
+++ 06_square-struct.go
@@ -2,40 +2,43 @@
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+)
 
 type Square struct {
-	x      int
-	y      int
-	length int
+	X      int
+	Y      int
+	Length int
 }
 
 func NewSquare(x int, y int, length int) (*Square, error) {
 	if length <= 0 {
-		return nil, fmt.Errorf("length should be >= 0")
+		return nil, fmt.Errorf("length should be > 0")
 	}
 	s := Square{x, y, length}
 	return &s, nil
 }
 
 func (s *Square) move(distX int, distY int) {
-	s.x += distX
-	s.y += distY
+	s.X += distX
+	s.Y += distY
 }
 
-func (s *Square) area() (area int) {
-	return s.length * s.length
+func (s *Square) area() int {
+	return s.Length * s.Length
 }
 
 func main() {
 	s, err := NewSquare(10, 1, 5)
 	if err != nil {
-		fmt.Printf("ERROR: %s\n", err)
+		log.Fatalf("ERROR: %s\n", err)
 	} else {
 		fmt.Println(s)
 	}
 
 	s.move(1, 5)
-	fmt.Println(s)
+	fmt.Printf("%+v\n", s)
 	fmt.Printf("Area: %v", s.area())
 }
