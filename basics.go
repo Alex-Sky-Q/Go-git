152e818510696b7fdc4d8af5f3d877816366bb6b
AlexSky <83851934+Alex-Sky-Q@users.noreply.github.com>
2022-10-07 00:58:45 +0300

    Task is solved with tests

--- basics.go
+++ basics.go
@@ -2,18 +2,23 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 	"strings"
 	"unicode"
 )
 
 func main() {
-	sent := "hello world. with pleasure"
+	sent := "hi.see you. "
 	fmt.Println(CorrectSent(sent))
 }
 
 // Task 1. Write a function that:
 // - converts first letter of a sentence to a capital letter
 // - adds a dot at the end
+// Assumptions:
+//- "x.z" (no space after dot) - z is not a new sentence
+//- "!", "?" at the end do not require additional dot
+
 func CorrectSent(s string) (res string) {
 	////var temp string
 	//temp := strings.ToUpper(string(s[0]))
@@ -24,25 +29,42 @@ func CorrectSent(s string) (res string) {
 	//	temp += string(l)
 	//}
 	//return temp
+
 	if strings.TrimSpace(s) == "" {
 		return s
 	}
-	var temp []string
-	strSplit := strings.Split(s, ". ")
 
-	for _, t := range strSplit {
-		for _, l := range t {
-			if unicode.IsLetter(l) {
-				t = strings.Replace(t, string(l), strings.ToUpper(string(l)), 1)
-				break
-			}
+	firstInd := 0
+	for i, l := range s {
+		if unicode.IsLetter(l) {
+			res = strings.Replace(s, string(l), strings.ToUpper(string(l)), 1)
+			firstInd = i
+			break
 		}
-		temp = append(temp, t)
 	}
 
-	res = strings.Join(temp, ". ")
+	sentLoc, _ := regexp.Compile("[.?!]\\s+")
+	loc := sentLoc.FindStringIndex(res[firstInd:])
+	if len(loc) >= 2 {
+		if loc[1] != len(res) {
+			res = res[:loc[1]] + strings.ToUpper(string(res[loc[1]])) + res[loc[1]+1:]
+		}
+	}
 
-	if string(res[len(res)-1]) != "." {
+	//var temp []string
+	//strSplit := strings.Split(s, ". ")
+	//for _, t := range strSplit {
+	//	for _, l := range t {
+	//		if unicode.IsLetter(l) {
+	//			t = strings.Replace(t, string(l), strings.ToUpper(string(l)), 1)
+	//			break
+	//		}
+	//	}
+	//	temp = append(temp, t)
+	//}
+	//res = strings.Join(temp, ". ")
+
+	if !strings.ContainsAny(string(res[len(res)-1]), ".!?") {
 		res += "."
 	}
 	return res
